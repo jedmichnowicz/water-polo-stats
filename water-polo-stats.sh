94407c317596ba4ab19bf7e37d1b06fbaa4971e0
Jed Michnowicz <jmichnow@adobe.com>
2024-09-21 14:40:54 -0700

    sample CSV files

--- water-polo-stats.sh
+++ water-polo-stats.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "TEAM,LEVEL,PLACING,GOALS_FOR_PER_GAME,GOALS_AGAINST_AGAINST_PER_GAME,POINT_DELTA,COMP_RANK,WINS,LOSSES,TIES"
+echo "TEAM,LEVEL,PLACING,GOALS FOR PER GAME,GOALS AGAINST AGAINST PER GAME,POINT DELTA,SCHEDULE STRENGTH,WINS,LOSSES,TIES,TIE WINS,TIE LOSSES"
 
 IFS='
 '
@@ -33,15 +33,8 @@ for TEAM in $(echo "$TEAMS" | sort -u); do
         WHITE_SCORE=$(echo "$SCORE" | awk -F',' '{print $2}' | sed -e 's|\..*||')
         DARK_SCORE=$(echo "$SCORE" | awk -F',' '{print $4}' | sed -e 's|\..*||')        
         GAME_COUNT=$((1 + $GAME_COUNT))
-
-        #if [ "$DARK_SCORE" = "$WHITE_SCORE" ]; then
-        #    TIES=$(($TIES + 1))
-        #    WHITE_TIE=$(echo "$SCORE" | awk -F',' '{print $2}' | awk -F'.' '{print $2}')
-        #    DARK_TIE=$(echo "$SCORE" | awk -F',' '{print $4}' | awk -F'.' '{print $2}')
-        #    if [ "$WHITE_TIE" -gt "$BLACK_TIE" ]; then
-        #        
-        #    fi
-        #fi
+        WHITE_TIE=$(echo "$SCORE" | awk -F',' '{print $2}' | awk -F'.' '{print $2}') 
+        DARK_TIE=$(echo "$SCORE" | awk -F',' '{print $4}' | awk -F'.' '{print $2}')
 
         if [ "$TEAM" = "$WHITE" ]; then
             GOALS_FOR=$(($GOALS_FOR + $WHITE_SCORE))
@@ -51,12 +44,18 @@ for TEAM in $(echo "$TEAMS" | sort -u); do
                 LOSSES=$(($LOSSES + 1))
             else
                 TIES=$(($TIES + 1))
+                if [ "$WHITE_TIE" -gt "$DARK_TIE" ]; then
+                    TIE_WINS=$((TIE_WINS + 1))
+                else
+                    TIE_LOSSES=$((TIE_LOSSES + 1))  
+                fi
             fi
         else
             GOALS_AGAINST=$(($GOALS_AGAINST + $WHITE_SCORE))
             PLACING=$(grep "$WHITE" "$2" | awk -F',' '{print $1}' | sed -e "s|.*_||")
             COMP_RANK=$(($COMP_RANK + $PLACING))
         fi
+
         if [ "$TEAM" = "$DARK" ]; then
             GOALS_FOR=$(($GOALS_FOR + $DARK_SCORE))
             if [ "$DARK_SCORE" -gt "$WHITE_SCORE" ]; then
@@ -65,6 +64,11 @@ for TEAM in $(echo "$TEAMS" | sort -u); do
                 LOSSES=$(($LOSSES + 1))
             else
                 TIES=$(($TIES + 1))
+                if [ "$DARK_TIE" -gt "$WHITE_TIE" ]; then
+                    TIE_WINS=$((TIE_WINS + 1))
+                else
+                    TIE_LOSSES=$((TIE_LOSSES + 1))  
+                fi                
             fi            
         else
             GOALS_AGAINST=$(($GOALS_AGAINST + $DARK_SCORE))
@@ -78,7 +82,7 @@ for TEAM in $(echo "$TEAMS" | sort -u); do
     GOALS_AGAINST_AGAINST_PER_GAME=$(echo "scale=2; $GOALS_AGAINST / $GAME_COUNT" | bc)
     DELTA=$(echo "scale=2; $GOALS_FOR_PER_GAME - $GOALS_AGAINST_AGAINST_PER_GAME" | bc)
     COMP_RANK_COMPUTED=$(echo "scale=2; $COMP_RANK / $GAME_COUNT" | bc)    
-    echo "$TEAM,$LEVEL,$PLACING,$GOALS_FOR_PER_GAME,$GOALS_AGAINST_AGAINST_PER_GAME,$DELTA,$COMP_RANK_COMPUTED,$WINS,$LOSSES,$TIES"
+    echo "$TEAM,$LEVEL,$PLACING,$GOALS_FOR_PER_GAME,$GOALS_AGAINST_AGAINST_PER_GAME,$DELTA,$COMP_RANK_COMPUTED,$WINS,$LOSSES,$TIES,$TIE_WINS,$TIE_LOSSES"
     
 done
 
